48046bead555685506d05e84ae1baccf3ee39271
Alexandre Marcireau <alex@localhost.localdomain>
2017-02-08 16:11:09 +0100

    call master on setup and animate the transition

--- quantumSwitch/quantumSwitch.ino
+++ quantumSwitch/quantumSwitch.ino
@@ -4,129 +4,152 @@
 #endif
 #include <WiFiManager.h>
 
-#define H_STRAND_PIN D1
-#define L_STRAND_PIN D2
+/// parameters
+const unsigned int serverResponseTimeout = 3000; // milliseconds
+const unsigned int masterConnectionRetryPeriod = 1000; // milliseconds
+const IPAddress masterIpAddress(192, 168, 0, 11);
+const int masterPort = 3000;
+const int animationDuration = 1000; // milliseconds
+const int animationFrameDuration = 20; // milliseconds
 
-const IPAddress serverIP(134, 157, 180, 144);
-const int port = 3003;
+/// globals
+Adafruit_NeoPixel topStrip = Adafruit_NeoPixel(12, D1);
+Adafruit_NeoPixel bottomStrip = Adafruit_NeoPixel(12, D2);
+WiFiServer internalServer(3001);
+bool isOpen = true;
 
-Adafruit_NeoPixel strip_h = Adafruit_NeoPixel(12, H_STRAND_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_l = Adafruit_NeoPixel(12, L_STRAND_PIN, NEO_GRB + NEO_KHZ800);
-
-WiFiServer server(80);
-bool state = true;
-unsigned int serverResponseTimeout = 5000;
-
-// Fill the dots one after the other with a color
-void setHighColor(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip_h.numPixels(); i++) {
-    strip_h.setPixelColor(i, c);
-    strip_h.show();
-    delay(wait);
-  }
-}
-
-
-void setLowColor(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip_h.numPixels(); i++) {
-    strip_l.setPixelColor(i, c);
-    strip_l.show();
-    delay(wait);
-  }
-}
-
-
-void setLightSignal(bool FREE){
-    uint32_t color;
-    if(FREE) {
-      color = strip_h.Color(255, 0, 0);
+/// fill sets the color of each LED on each strip.
+void fill(unsigned char r, unsigned char  g, unsigned char  b) {
+    {
+        uint32_t color = topStrip.Color(r, g, b);
+        for(uint16_t ledIndex = 0; ledIndex < topStrip.numPixels(); ++ledIndex) {
+            topStrip.setPixelColor(ledIndex, color);
+        }
     }
-    else {
-      color = strip_h.Color(0, 255, 0);
+    {
+        uint32_t color = bottomStrip.Color(r, g, b);
+        for(uint16_t ledIndex = 0; ledIndex < bottomStrip.numPixels(); ++ledIndex) {
+            bottomStrip.setPixelColor(ledIndex, color);
+        }
     }
-    setHighColor(color, 0);
-    setLowColor(color, 0);
+    topStrip.show();
+    bottomStrip.show();
 }
 
-
+/// setup is called once on startup.
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("quantiChiotsSwitch");
-  Serial.println(WiFi.SSID());
-  strip_l.begin();
-  strip_h.begin();
-  strip_l.setBrightness(255);
-  strip_l.setBrightness(255);
-  strip_l.show();
-  strip_h.show();
-  server.begin();
-  Serial.println("Server started");
-  // Get initial state
-  WiFiClient client;
-  Serial.println("Getting door status...");
-  do {
-    delay(50);
-  } while(!client.connect(serverIP, port));
-  client.println("GET /status/ HTTP/1.1");
-  client.println(String("Host: ") + serverIP);
-  client.println("Connection: close");
-  client.println();
-      
-  // wait for response
-  unsigned long now = millis();
-  while (client.available() == 0) {
-    if (millis() - now > 5000) {
-      Serial.println("timeout");
-      client.stop();
-      return;
-      }
-    delay(100);
+
+    // connect to the wifi
+    Serial.begin(115200);
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("xHotsWifi_setup");
+    Serial.print("connected to the access point with ip ");
+    Serial.println(WiFi.localIP());
+
+    // start the internal server
+    internalServer.begin();
+    internalServer.setNoDelay(true);
+    Serial.println(String("internal server listening on port ") + String(internalServerPort));
+
+    // retrieve the current door status
+    {
+        WiFiClient client;
+        for (;;) {
+            if (client.connect(masterIpAddress, masterPort)) {
+                Serial.println('connected to master, sending a request to get the current door state');
+                client.println("GET / HTTP/1.1");
+                client.println(String("Host: ") + masterIpAddress.toString() + ":" + String(masterPort));
+                client.println("Connection: close");
+                client.println();
+                bool stateSet = false;
+                unsigned long now = millis();
+                while (client.connected()) {
+                    if (millis() - now > serverResponseTimeout) {
+                        break;
+                    }
+                    if (client.available() > 0) {
+                        isOpen = !(client.readStringUntil('\r').substring(13) == "closed");
+                        if (isOpen) {
+                            fill(0, 255, 0);
+                        } else {
+                            fill(255, 0, 0);
+                        }
+                        delay(animationFrameDuration);
+                        stateSet = true;
+                        break;
+                    }
+                    if (stateSet) {
+                        break;
+                    }
+                }
+            } else {
+                Serial.println('connection to master failed');
+                delay(masterConnectionRetryPeriod);
+            }
+        }
+
     }
-  Serial.println("success");
-  // read the response
-  Serial.println("Response:");
-  String response = "";
-  while (client.available() > 0) {
-    response += client.readStringUntil('\r');
-    //Serial.print(String("    ") + client.readStringUntil('\r'));
-  }
-  Serial.println(response);
-  client.stop();
-  if(response.indexOf("open") != -1){
-    setLightSignal(false);
-  }
-  if(response.indexOf("closed") != -1){
-    setLightSignal(true);
-  }
 }
 
+/// loop is called repeatedly while the chip is on.
 void loop() {
-    WiFiClient client = server.available();
+    WiFiClient client = internalServer.available();
     if (client) {
-		unsigned long now = millis();
-		while (client.connected()) {
-			if (millis() - now > serverResponseTimeout) {
-				client.println("HTTP/1.1 200 OK\r\n");
-			    client.stop();
-			    break;
-			}
-			if(client.available()){
-				String requestPart = client.readStringUntil('\n');
-				if(requestPart.indexOf("closed") != -1) {
-					setLightSignal(true);
-					Serial.println("Just got trigger from master --> close");
-					break;
-				}
-				if(requestPart.indexOf("open") != -1) {
-					setLightSignal(false);
-					Serial.println("Just got trigger from master --> open");
-					break;
-				}
-			}
-		}
-		client.println("HTTP/1.1 200 OK \n\r");
-		client.stop();
-	}
-}   	   
+        unsigned long now = millis();
+        while (client.connected()) {
+            if (millis() - now > serverResponseTimeout) {
+                client.println("HTTP/1.1 422 Unprocessable Entity\r\n");
+                client.stop();
+                break;
+            }
+            if (client.available()) {
+                String requestPart = client.readStringUntil('\n');
+                if (requestPart.substring(0, 7) == "status=") {
+                    if (isOpen != !(requestPart.substring(7) == "closed")) {
+                        isOpen = !isOpen;
+                        for (unsigned int frameIndex = 0; frameIndex <= animationDuration / animationFrameDuration; ++frameIndex) {
+                            float progress = (float)frameIndex / (animationDuration / animationFrameDuration);
+                            if (progress < 0.5) {
+                                if (isOpen) {
+                                    fill(255, (unsigned char)(510 * progress), 0);
+                                } else {
+                                    fill((unsigned char)(510 * progress), 255, 0);
+                                }
+                            } else {
+                                if (isOpen) {
+                                    fill((unsigned char)(510 * (1 - progress)), 255, 0);
+                                } else {
+                                    fill(255, (unsigned char)(510 * (1 - progress)), 0);
+                                }
+                            }
+                            delay(animationFrameDuration);
+                        }
+                    }
+                } else if (requestPart.substring(0, 5) == "path=") {
+                    String path = requestPart.substring(5);
+                    Serial.println(String("will reboot using the firmware located at '") + path + "'");
+                    switch(ESPhttpUpdate.update(path)) {
+                        case HTTP_UPDATE_FAILED:
+                            Serial.println(String("http-update failed with error '") + ESPhttpUpdate.getLastErrorString().c_str() + "' (" + String(ESPhttpUpdate.getLastError()) + ")");
+                            client.println(
+                                String("HTTP/1.1 200 http update failed with error '")
+                                + ESPhttpUpdate.getLastErrorString().c_str()
+                                + "' ("
+                                + String(ESPhttpUpdate.getLastError())
+                                + ")\r\n"
+                            );
+                            break;
+                        case HTTP_UPDATE_NO_UPDATES:
+                            Serial.println("http-update did nothing");
+                            client.println("HTTP/1.1 200 http-update did nothing\r\n");
+                            break;
+                        case HTTP_UPDATE_OK:
+                            Serial.println("http-update successful\r\n");
+                            break;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
