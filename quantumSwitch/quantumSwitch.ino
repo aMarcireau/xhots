a704025050db3aeec85371206e1afa1adac7ceb7
samp38 <samuel.heidmann@gmail.com>
2017-03-07 12:12:39 +0100

    Add auto shutdown from 21h00 to 7h45, based on NTP time server

--- quantumSwitch/quantumSwitch.ino
+++ quantumSwitch/quantumSwitch.ino
@@ -5,6 +5,8 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <ESP8266httpUpdate.h>
+#include <TimeLib.h>
+#include <WiFiUdp.h>
 
 /// parameters
 const unsigned int serverResponseTimeout = 3000; // milliseconds
@@ -14,12 +16,18 @@ const int masterPort = 3000;
 const int animationDuration = 1000; // milliseconds
 const int animationFrameDuration = 20; // milliseconds
 const unsigned int internalServerPort = 3001;
+static const char ntpServerName[] = "us.pool.ntp.org"; // NTP server to check time
+const int timeZone = 1; // East European time
+unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 /// globals
+WiFiUDP udp;
 Adafruit_NeoPixel topStrip = Adafruit_NeoPixel(12, D1);
 Adafruit_NeoPixel bottomStrip = Adafruit_NeoPixel(12, D2);
 WiFiServer internalServer(internalServerPort);
 bool isOpen = true;
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 
 /// fill sets the color of each LED on each strip.
 void fill(unsigned char r, unsigned char g, unsigned char b) {
@@ -37,6 +45,83 @@ void fill(unsigned char r, unsigned char g, unsigned char b) {
     }
 	topStrip.show();
 	bottomStrip.show();
+}
+
+/// getNtpTile retrieves the current time from a server.
+time_t getNtpTime() {
+    IPAddress ntpServerIP; // NTP server's ip address
+
+    while (Udp.parsePacket() > 0) {} // discard any previously received packets
+    Serial.println("Transmit NTP Request");
+
+    WiFi.hostByName(ntpServerName, ntpServerIP);
+    Serial.print(ntpServerName);
+    Serial.print(": ");
+    Serial.println(ntpServerIP);
+    sendNTPpacket(ntpServerIP);
+    uint32_t beginWait = millis();
+    while (millis() - beginWait < 1500) {
+        int size = Udp.parsePacket();
+        if (size >= NTP_PACKET_SIZE) {
+            Serial.println("Receive NTP Response");
+            Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+            unsigned long secsSince1900;
+            // convert four bytes starting at location 40 to a long integer
+            secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+            secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+            secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+            secsSince1900 |= (unsigned long)packetBuffer[43];
+            return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+        }
+    }
+    Serial.println("No NTP Response :-(");
+    return 0; // return 0 if unable to get the time
+}
+
+/// sendNtpPacket sends an NTP request to the time server at the given address.
+void sendNtpPacket(IPAddress& address) {
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    Udp.beginPacket(address, 123); //NTP requests are to port 123
+    Udp.write(packetBuffer, NTP_PACKET_SIZE);
+    Udp.endPacket();
+}
+
+/// printDigits is used by digitalClockDisplay.
+void printDigits(int digits) {
+    Serial.print(":");
+    if (digits < 10) {
+        Serial.print('0');
+    }
+    Serial.print(digits);
+}
+
+/// digitalClockDisplay prints the retrieved time in the terminal.
+void digitalClockDisplay() {
+    // digital clock display of the time
+    Serial.print(hour());
+    printDigits(minute());
+    printDigits(second());
+    Serial.print(" ");
+    Serial.print(day());
+    Serial.print(".");
+    Serial.print(month());
+    Serial.print(".");
+    Serial.print(year());
+    Serial.println();
 }
 
 /// setup is called once on startup.
@@ -65,6 +150,15 @@ void setup() {
     internalServer.begin();
     internalServer.setNoDelay(true);
     Serial.println(String("internal server listening on port ") + String(internalServerPort));
+
+    // Start UDP service to receive NTP packets
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Local port: ");
+    Serial.println(Udp.localPort());
+    Serial.println("waiting for sync");
+    setSyncProvider(getNtpTime);
+    setSyncInterval(3600);
 
     // retrieve the current door status
     {
@@ -111,6 +205,7 @@ void setup() {
 
 /// loop is called repeatedly while the chip is on.
 void loop() {
+<<<<<<< HEAD
     WiFiClient client = internalServer.available();
     if (client) {
         unsigned long now = millis();
@@ -171,6 +266,25 @@ void loop() {
                     break;
                 }
             }
+        }
+    }
+
+    // Manage auto shutdown
+    if (hour() == 21) {
+        for (unsigned int i = 0; i <= 255; i++) {
+            fill((unsigned char)(255 - i), (unsigned char)(255 - i), (unsigned char)(0));
+            delay(animationFrameDuration);
+        }
+        if (weekday() > 0 && weekday() < 5) {
+            // Turn off for the night
+            Serial.println("Going to sleep for the night");
+            delay(500);
+            ESP.deepSleep(38700 * 1000000); // 10h45 of sleep Time --> will wake up at 7h45 AM
+        } else {
+            // Turn off for the weekend
+            Serial.println("Going to sleep for the week end");
+            delay(500);
+            ESP.deepSleep(211500 * 1000000);
         }
     }
 }
